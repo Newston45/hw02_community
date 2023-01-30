112b60ec573b7bd325954425bc29e80f5d33ac72
Михаил Иванов <newston45@gmail.com>
2023-01-30 14:47:59 +0300

    according to PEP8

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class Group(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=100, unique=True)
@@ -22,9 +23,8 @@ class Post(models.Model):
     )
     group = models.ForeignKey(
         Group,
-        blank = True,
-        null = True,
-        on_delete = models.CASCADE,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
         related_name='group'
     )
-
