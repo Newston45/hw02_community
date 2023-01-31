9c302550391772d254cf44f9678995064ff2b3cf
Михаил Иванов <newston45@gmail.com>
2023-01-31 11:39:14 +0300

    delete unusefull comments

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -2,16 +2,10 @@ from django.contrib import admin
 from .models import Post, Group
 
 
-# При регистрации модели Post источником конфигурации для неё назначаем
-# класс PostAdmin
 class PostAdmin(admin.ModelAdmin):
-    # Перечисляем поля, которые должны отображаться в админке
     list_display = ('pk', 'text', 'pub_date', 'author', 'group',)
-    # Добавляем интерфейс для поиска по тексту постов
     search_fields = ('text',)
-    # Добавляем возможность фильтрации по дате
     list_filter = ('pub_date',)
-    # Это свойство сработает для всех колонок: где пусто — там будет эта строка
     empty_value_display = '-пусто-'
     list_editable = ('group',)
 
