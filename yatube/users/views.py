f68ac295632ab7daea740d04ff6d1c6461e147ee
Newston45 <newston45@gmail.com>
2023-04-07 14:33:03 +0300

    added navbar

--- yatube/users/views.py
+++ yatube/users/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import CreateView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
 from .forms import ContactForm
+
 
 # Функция reverse_lazy позволяет получить URL по параметрам функции path()
 # Берём, тоже пригодится
@@ -12,12 +14,16 @@ from .forms import CreationForm
 
 class SignUp(CreateView):
     form_class = CreationForm
-    success_url = reverse_lazy('posts:index')
+    success_url = reverse_lazy('posts:profile')
     template_name = 'users/signup.html'
 
 
+def logout_view(request):
+    logout(request)
+    return redirect('/logout/')
+
+
 def user_contact(request):
-    ...
     # Создаём объект формы
     form = ContactForm()
 
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -19,8 +19,8 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
-    path('contact/', include('users.urls')),
     path('auth/', include('users.urls')),
     path('auth/', include('django.contrib.auth.urls')),
+    path('contact/', include('users.urls')),
     path('about/', include('about.urls', namespace='about')),
 ]
